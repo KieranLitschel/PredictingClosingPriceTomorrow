450c1830f90713a0189214aaf87a547ae072a829
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-06-18 16:43:08 +0100

    Finished addNewStock

--- Database.py
+++ Database.py
@@ -1,8 +1,9 @@
 import bs4 as bs
 import requests
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import MySQLConnection, Error
 import AlphaVantageAPI as AVAPI
+import datetime
 
 
 # Modified version of method from https://pythonprogramming.net/sp500-company-list-python-programming-for-finance/
@@ -18,20 +19,51 @@ def getSP500Tickers():
     return tickersNSectors
 
 
+def pointToDate(point):
+    dateComps = str(point).split('-')
+    date = datetime.date(int(dateComps[0]), int(dateComps[1]), int(dateComps[2]))
+    return date
+
+
 class DBManager:
     def __init__(self, apiKey, pwrd):
         self.av = AVAPI.AlphaVantage(apiKey)
-        # Connect to the database
+        self.pwrd = pwrd
+
+    def insert(self, query, args, multi):
         try:
-            self.conn = mysql.connector.connect(host='localhost', database='stocks', user='root', password=pwrd)
+            conn = mysql.connector.connect(host='localhost', database='stocks', user='root', password=self.pwrd)
+            cursor = conn.cursor()
+            if multi:
+                cursor.executemany(query, args)
+            else:
+                cursor.execute(query, args)
+            conn.commit()
         except Error as e:
             print(e)
+        finally:
+            cursor.close()
+            conn.close()
 
     def addNewStock(self, ticker, sector):
         history = self.av.getDailyHistory(AVAPI.OutputSize.FULL, ticker)
         points = list(history.keys())
-        firstDay = points[-1]
-        lastDay = points[0]
-        sql = "INSERT INTO tickers(ticker,sector,firstDay,lastDay) VALUES({0},{1},{2},{3}); ".format(ticker, sector,
-                                                                                                         firstDay,
-                                                                                                         lastDay)
+        firstDay = pointToDate(points[-1])
+        lastDay = pointToDate(points[0])
+        query = "INSERT INTO tickers(ticker,sector,firstDay,lastDay) " \
+                "VALUES(%s,%s,DATE(%s),DATE(%s))"
+        args = (ticker, sector, firstDay, lastDay)
+        self.insert(query, args, False)
+        args = []
+        for point in points:
+            pointInHistory = history.get(point)
+            date = pointToDate(point)
+            open = float(pointInHistory.get('1. open'))
+            high = float(pointInHistory.get('2. high'))
+            low = float(pointInHistory.get('3. low'))
+            close = float(pointInHistory.get('4. close'))
+            volume = int(pointInHistory.get('5. volume'))
+            args.append((ticker, date, open, high, low, close, volume))
+        query = "INSERT INTO timeseriesdaily(ticker,date,open,high,low,close,volume) " \
+                "VALUES(%s,DATE(%s),%s,%s,%s,%s,%s)"
+        self.insert(query, args, True)
