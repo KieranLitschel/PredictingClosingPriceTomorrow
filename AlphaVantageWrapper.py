9c2b3f577ea5023bab15cf687c8d1bb714691e96
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-06-22 20:22:17 +0100

    Added method to update all stocks and improved code readability in existing functions

--- AlphaVantageWrapper.py
+++ AlphaVantageWrapper.py
@@ -1,13 +1,14 @@
 import enum
 import requests
+import time
 
 
 class AlphaVantage:
     def __init__(self, apiKey):
         self.apiKey = apiKey
 
-    def getDailyHistory(self, outputsize, ticker):
-        if outputsize == OutputSize.FULL:
+    def requestDailyHistory(self, outputSize, ticker):
+        if outputSize == OutputSize.FULL:
             response = requests.get(
                 "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={0}&outputsize=full&apikey={1}".format(
                     ticker, self.apiKey))
@@ -17,6 +18,19 @@ class AlphaVantage:
                                                                                                             self.apiKey))
         history = response.json().get('Time Series (Daily)')
         return history
+
+    def getDailyHistory(self, outputSize, ticker):
+        history = self.requestDailyHistory(outputSize, ticker)
+        # The API sometimes does not return the response we require if its overloaded, in which case we wait a bit
+        # and try again
+        while history is None:
+            print('Last request failed. Retrying...')
+            time.sleep(1.5)
+            history = self.getDailyHistory(outputSize, ticker)
+            if not (history is None):
+                print('Request succeeded.')
+        return history
+
 
 class OutputSize(enum.Enum):
     FULL = enum.auto()
