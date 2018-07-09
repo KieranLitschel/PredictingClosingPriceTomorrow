e6afd6ef56baf4c7f3c2861b715c7f6fcd65cb09
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-07-09 16:41:51 +0100

    Added a method to allow for a range of nearest neighbours to be investigated at once

--- Predictor.py
+++ Predictor.py
@@ -1,23 +1,50 @@
-import Database
+import time
 from sklearn.neighbors import KNeighborsClassifier
 
 
 class Classifier:
-    def __init__(self, apiKey, pwrd):
-        self.db = Database.DBManager(apiKey, pwrd)
+    def __init__(self, trainX, trainY, testX, testY=None):
+        self.trainX = trainX
+        self.trainY = trainY
+        self.testX = testX
+        self.testY = testY
 
-    def classifyByKnn(self, k, trainX, trainY, testX, testY=None):
-        neigh = KNeighborsClassifier(n_neighbors=k, algorithm="auto", n_jobs=-1)
-        print("Fitting training data...")
-        neigh.fit(trainX, trainY)
-        print("Predicting test data...")
-        predictedY = neigh.predict(testX)
-        if testY is None:
-            return predictedY
+    def classifyByKnnInRange(self, ks, returnPredictions=False, returnAccuracy=True, printProgress=True, printTime=True):
+        results = {}
+        for i in ks:
+            print('Trying k=%s' % i)
+            start = time.time()
+            result = self.classifyByKnn(i, returnPredictions, returnAccuracy, printProgress=False, coresToUse=4)
+            if printTime:
+                print('Took %.1f seconds.' % (time.time()-start))
+            results[i] = result
+            if printProgress:
+                if returnPredictions and returnAccuracy:
+                    print("k=%s gave accuracy of %.4f%%" % (i, result[i] * 100))
+                else:
+                    print("k=%s gave accuracy of %.4f%%" % (i, result * 100))
+        return results
+
+    def classifyByKnn(self, k, returnPredictions=True, returnAccuracy=False, printProgress=True, coresToUse=-1):
+        neigh = KNeighborsClassifier(n_neighbors=k, algorithm="auto", n_jobs=coresToUse) # Using ball_tree as kd_tree runs out of memory
+        if printProgress:
+            print("Fitting training data...")
+        neigh.fit(self.trainX, self.trainY)
+        if printProgress:
+            print("Predicting test data...")
+        if returnPredictions:
+            predictedY = neigh.predict(self.testX)
+            if not returnAccuracy:
+                return predictedY
+            else:
+                correct = 0
+                for i in range(0, len(predictedY)):
+                    if predictedY[i] == self.testY[i]:
+                        correct += 1
+                accuracy = correct / len(predictedY)
+                if returnPredictions:
+                    return accuracy, predictedY
+                else:
+                    return accuracy
         else:
-            correct = 0
-            for i in range(0, len(predictedY)):
-                if predictedY[i] == testY[i]:
-                    correct += 1
-            accuracy = correct / len(predictedY)
-            return accuracy, predictedY
+            return neigh.score(self.testX, self.testY)
