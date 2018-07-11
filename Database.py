145d017b8655407298829fd5a294b98918b2ea16
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-07-11 21:08:04 +0100

    Updated function to calculate bollinger bands to give more useful features

--- Database.py
+++ Database.py
@@ -50,8 +50,8 @@ class DBManager:
     def __init__(self, apiKey, pwrd):
         self.av = AVW.AlphaVantage(apiKey)
         self.pwrd = pwrd
-        self.insertAllTSDQuery = "INSERT INTO timeseriesdaily(ticker,date,dateTmrw,open,high,low,close,adjClose,volume,adjClosePChange,pDiffClose5SMA,pDiffClose8SMA,pDiffClose13SMA,rsi,pDiffClose22BB) " \
-                                 "VALUES(%s,DATE(%s),DATE(%s),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        self.insertAllTSDQuery = "INSERT INTO timeseriesdaily(ticker,date,dateTmrw,open,high,low,close,adjClose,volume,adjClosePChange,pDiffClose5SMA,pDiffClose8SMA,pDiffClose13SMA,rsi,pDiffCloseUpperBB, pDiffCloseLowerBB, pDiff20SMAAbsBB) " \
+                                 "VALUES(%s,DATE(%s),DATE(%s),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
     def insert(self, query, args, many=False):
         try:
@@ -316,10 +316,11 @@ class DBManager:
                 pDiffClose5SMA = fc.smaPDiff(closeHist, 5)
                 pDiffClose8SMA = fc.smaPDiff(closeHist, 8)
                 pDiffClose13SMA = fc.smaPDiff(closeHist, 13)
-                pDiffClose22BB = fc.bollingerBandsPDiff(closeHist,20,2)
+                pDiffCloseUpperBB, pDiffCloseLowerBB, pDiff20SMAAbsBB = fc.bollingerBandsPDiff(closeHist, 20, 2)
                 rsi = fc.RSI(closeHist)
                 arg = [ticker, date, dateTmrw, open, high, low, close, adjClose, volume, adjClosePChange,
-                       pDiffClose5SMA, pDiffClose8SMA, pDiffClose13SMA, rsi, pDiffClose22BB]
+                       pDiffClose5SMA, pDiffClose8SMA, pDiffClose13SMA, rsi, pDiffCloseUpperBB, pDiffCloseLowerBB,
+                       pDiff20SMAAbsBB]
                 if fieldsToRestore is not None:
                     for column in columnNames:
                         value = None
@@ -379,7 +380,7 @@ class DBManager:
         print("Restoring tickers and sectors...")
         with open('tickersNSectors.pickle', 'rb') as handle:
             tickersNSectors = pickle.load(handle)
-        self.insert("INSERT INTO tickers(ticker,sector) VALUES (%s,%s)",tickersNSectors,many=True)
+        self.insert("INSERT INTO tickers(ticker,sector) VALUES (%s,%s)", tickersNSectors, many=True)
         print("Restoring columns in timeseriesdaily...")
         with open('fieldsToRestore.pickle', 'rb') as handle:
             fieldsToRestore = pickle.load(handle)
@@ -387,8 +388,8 @@ class DBManager:
         columns = []
         for ticker in fieldsToRestore.keys():
             for date in fieldsToRestore[ticker].keys():
-                arg = [ticker,date]
-                if len(columns)==0:
+                arg = [ticker, date]
+                if len(columns) == 0:
                     columns = list(fieldsToRestore[ticker][date].keys())
                 for column in fieldsToRestore[ticker][date].keys():
                     arg.append(fieldsToRestore[ticker][date][column])
@@ -398,8 +399,8 @@ class DBManager:
         for column in columns:
             query += "," + column
             endQuery += ",%s"
-        query += endQuery+")"
-        self.insert(query,args,many=True)
+        query += endQuery + ")"
+        self.insert(query, args, many=True)
         print("Readded all columns")
 
     def readdAllStocks(self, columnsToSave=[]):
