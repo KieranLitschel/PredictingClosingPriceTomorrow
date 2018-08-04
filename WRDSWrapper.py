1df88bb758d932f2bb9ee2760e60b7d76ffba127
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-08-04 14:56:10 +0100

    Added method to get financial data

--- WRDSWrapper.py
+++ WRDSWrapper.py
@@ -6,7 +6,7 @@ class WRDS:
     def __init__(self, wrds_username):
         self.db = wrds.Connection(wrds_username=wrds_username)
 
-    def getPermno(self, tickers):
+    def getPermnos(self, tickers):
         query = "SELECT DISTINCT ticker, permno FROM crsp.dse WHERE"
         if type(tickers) is list:
             first = True
@@ -21,8 +21,26 @@ class WRDS:
         response = np.array(self.db.raw_sql(query))
         permnos = {}
         for row in response:
-            if permnos.get(row[0]) is None:
-                permnos[row[0]] = [row[1]]
-            else:
-                permnos[row[0]].append(row[1])
+            permnos[int(row[1])] = row[0]
         return permnos
+
+    def getFinancials(self, permnos, columns):
+        query = "SELECT permno,public_date"
+        if type(columns) is list:
+            for column in columns:
+                query += "," + column
+        else:
+            query += ","+columns
+        query += " FROM wrdsapps_finratios.firm_ratio WHERE "
+        if type(permnos) is list:
+            first = True
+            for permno in permnos:
+                if first:
+                    query += "permno="+str(permno)
+                    first = False
+                else:
+                    query += " OR permno="+str(permno)
+        else:
+            query += "permno="+permnos
+        financials = np.array(self.db.raw_sql(query))
+        return financials
